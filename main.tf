16ed59e7796963a22fb0a1bfba1c61af0a55123e
cfculler <carsonculler2020@gmail.com>
2023-09-28 13:05:36 -0700

    apparently ID's aren't sensitive info

--- main.tf
+++ main.tf
@@ -15,9 +15,9 @@ terraform {
     container_name       = "carson-dob"
     key                  = "prod.terraform.carson-dob"
     use_oidc             = true
-    subscription_id      = var.az_subscription_id
-    tenant_id            = var.az_tenant_id
-    client_id            = var.az_client_id
+    subscription_id      = "3e16852e-8399-4c16-b246-16bf46bc3747"
+    tenant_id            = "1b4a4fed-fed8-4823-a8a0-3d5cea83d122"
+    client_id            = "e886df3c-c276-43de-a390-d1cdc6d69e57"
   }
 }
 
