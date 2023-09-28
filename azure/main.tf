e9fa75bf7c8d222d4ed49c67d8bfb857daa583cd
cfculler <carsonculler2020@gmail.com>
2023-09-28 16:24:34 -0700

    converted to module

---
+++ azure/main.tf
@@ -0,0 +1,34 @@
+resource "azurerm_resource_group" "rg" {
+  name     = "carson-terraform-dob"
+  location = "westus2"
+  tags = {
+    Environment = "Terraform Getting Started"
+    Team = "DevOps"
+  }
+}
+
+# Create a virtual network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "carson-terraform-dob-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = "westus2"
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_storage_account" "sa" {
+  name                     = "carsonterraformstorage"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+  tags = {
+    environment = "staging"
+  }
+}
+
+resource "azurerm_storage_container" "sc" {
+  name                  = "carson-dob"
+  storage_account_name  = azurerm_storage_account.sa.name
+  container_access_type = "private"
+}
