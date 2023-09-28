1991d037e1d72986e982f3fabda936aaaa605294
cfculler <carsonculler2020@gmail.com>
2023-09-28 14:23:22 -0700

    testing

--- main.tf
+++ main.tf
@@ -14,15 +14,14 @@ terraform {
     storage_account_name = "carsonterraformstorage"
     container_name       = "carson-dob"
     key                  = "prod.terraform.carson-dob"
-    use_oidc             = true
-    subscription_id      = "3e16852e-8399-4c16-b246-16bf46bc3747"
-    tenant_id            = "1b4a4fed-fed8-4823-a8a0-3d5cea83d122"
-    client_id            = "e886df3c-c276-43de-a390-d1cdc6d69e57"
   }
 }
 
 provider "azurerm" {
   features {}
+  subscription_id      = "3e16852e-8399-4c16-b246-16bf46bc3747"
+  tenant_id            = "1b4a4fed-fed8-4823-a8a0-3d5cea83d122"
+  client_id            = "e886df3c-c276-43de-a390-d1cdc6d69e57"
 }
 
 resource "azurerm_resource_group" "rg" {
